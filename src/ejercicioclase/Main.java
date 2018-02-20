e71b005228d6c4f3d4a82abbe506efe78f8740ec
Unknown <carlosdavid_229@hotmail.com>
2018-02-20 08:25:05 -0500

    Inicio
    Inicio del programa

--- src/ejercicioclase/Main.java
+++ src/ejercicioclase/Main.java
@@ -4,10 +4,9 @@
  * and open the template in the editor.
  */
 package ejercicioclase;
-
 /**
- *
- * @author user
+ * Programa realizado para la clase de Programacion II
+ * @author carlosgonzalez
  */
 public class Main {
 
@@ -15,7 +14,26 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        inicioTitulo();
+        
+        System.out.println("Clase V");        
+        System.out.println("Ejercicio 1 En Clase");
+        System.out.println("");
+        
+        int opc;
+        
+        System.out.println("Escoja la opcion del ejercicio que desea realizar.");
+        
+        
+        
     }
-    
+    public static void inicioTitulo(){
+        System.out.println(" ###                               ");
+        System.out.println("  #   #    #  #   ####   #   ####  ");
+        System.out.println("  #   ##   #  #  #    #  #  #    # ");
+        System.out.println("  #   # #  #  #  #       #  #    # ");
+        System.out.println("  #   #  # #  #  #       #  #    # ");
+        System.out.println("  #   #   ##  #  #    #  #  #    # ");
+        System.out.println(" ###  #    #  #   ####   #   ####  ");                             
+    }
 }
